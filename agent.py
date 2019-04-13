cca648959d4a748d3e5d06ad8445aa91536f72e8
justkim <kimiyasid@yahoo.com>
2019-04-13 14:52:53 +0430

    add project files

---
+++ agent.py
@@ -0,0 +1,45 @@
+import tensorflow as tf
+import numpy as np
+import gym
+import math
+import os
+
+import model
+import architecture as policies
+import mario_env as env
+
+# SubprocVecEnv creates a vector of n environments to run them simultaneously.
+from baselines.common.vec_env.subproc_vec_env import SubprocVecEnv
+
+
+def main():
+    config = tf.ConfigProto()
+    # Avoid warning message errors
+    # os.environ["CUDA_VISIBLE_DEVICES"]="0"
+    # # Allowing GPU memory growth
+    # In some cases it is desirable
+    # for the process to only allocate a subset of the available memory,
+    # or to only grow the memory usage as it is needed by the process.TensorFlow provides two configuration options on the session to control this.The first
+    # is the allow_growth option,
+    # which attempts to allocate only as much GPU memory based on runtime allocations, it starts out allocating very little memory,
+    # and as sessions get run and more GPU memory is needed, we extend the GPU memory region needed by the TensorFlow process.
+    # config.gpu_options.allow_growth = True
+    #[env.make_train_0,env.make_train_1,env.make_train_2,env.make_train_3,env.make_train_4,env.make_train_5,env.make_train_6,env.make_train_7]
+    #env.make_train_0,env.make_train_0,env.make_train_0,env.make_train_0,env.make_train_0,env.make_train_0,env.make_train_0,env.make_train_0
+    with tf.Session(config=config):
+        model.learn(policy=policies.A2CPolicy,
+                            env=SubprocVecEnv([env.make_train_0]),
+                            nsteps=2048,
+                            total_timesteps=1000000000,
+                            gamma=0.99,
+                            lam = 0.95,
+                            vf_coef=0.5,
+                            ent_coef=0.001,
+                            lr = 2e-4,
+                            max_grad_norm = 0.5,
+                            log_interval = 10
+                            )
+
+if __name__ == '__main__': #this is important.why?
+    main()
+
---
+++ architecture.py
@@ -0,0 +1,133 @@
+import numpy as np
+import tensorflow as tf
+
+
+
+# This function selects the probability distribution over actions
+from baselines.common.distributions import make_pdtype
+
+
+# Convolution layer
+def conv_layer(inputs, filters, kernel_size, strides, gain=1.0):
+    return tf.layers.conv2d(inputs=inputs,
+                            filters=filters,
+                            kernel_size=kernel_size,
+                            strides=(strides, strides),
+                            activation=tf.nn.elu,
+                            kernel_initializer=tf.contrib.layers.xavier_initializer_conv2d())
+
+
+# Fully connected layer
+def fc_layer(inputs, units, activation_fn=tf.nn.elu, gain=1.0,name1="defualt"):
+    return tf.layers.dense(inputs=inputs,
+                           units=units,
+                           activation=activation_fn,
+                           kernel_initializer=tf.contrib.layers.xavier_initializer(),name=name1
+                           )
+
+"""
+This object creates the A2C Network architecture
+"""
+
+
+class A2CPolicy(object):
+    def __init__(self, sess, ob_space, action_space, nbatch, nsteps, reuse=False):
+        # This will use to initialize our kernels
+        gain = np.sqrt(2)
+
+        # Based on the action space, will select what probability distribution type
+        # we will use to distribute action in our stochastic policy (in our case DiagGaussianPdType
+        # aka Diagonal Gaussian, 3D normal distribution
+        self.pdtype = make_pdtype(action_space)
+
+        height, weight, channel = ob_space.shape
+        ob_shape = (height, weight, channel)
+
+        # Create the input placeholder
+        inputs_ = tf.placeholder(tf.float32, [None, *ob_shape], name="input")
+
+        # Normalize the images
+        scaled_images = tf.cast(inputs_, tf.float32) / 255.
+
+        """
+        Build the model
+        3 CNN for spatial dependencies
+        Temporal dependencies is handle by stacking frames
+        (Something funny nobody use LSTM in OpenAI Retro contest)
+        1 common FC
+        1 FC for policy
+        1 FC for value
+        """
+        with tf.variable_scope("model", reuse=reuse):
+            conv1 = conv_layer(scaled_images, 32, 8, 4, gain)#8 8 4 32
+            conv2 = conv_layer(conv1, 64, 4, 2, gain) #4 4 32 64
+            conv3 = conv_layer(conv2, 64, 3, 1, gain)# 8 64 8 8
+            flatten1 = tf.layers.flatten(conv3,"FLAT") # 8 4096
+            fc_common = fc_layer(flatten1, 512,gain=gain,name1="FC512") # 8 512
+            self.p_layer=tf.layers.dense(inputs=fc_common,
+                           units=7,
+                           activation=tf.nn.elu,
+                           kernel_initializer=tf.orthogonal_initializer(gain=0.01),name="p_layer"
+                           )
+            # This build a fc connected layer that returns a probability distribution
+            # over actions (self.pd) and our pi logits (self.pi).
+            # self.pd, self.pi = self.pdtype.pdfromlatent(fc_common, init_scale=0.01)
+            softmax_layer=tf.nn.softmax(self.p_layer,name="softmax")
+            self.dist = tf.distributions.Categorical(probs=softmax_layer)
+            a0=self.dist.sample()
+            # entropy=self.dist.entropy("entropy")
+
+
+            #tf.clip_by_value(fc_common,1e-10,1.0)-
+            # self.pd, self.pi = self.pdtype.pdfromlatent(fc_common, init_scale=0.01)
+            # Calculate the v(s)
+            vf = fc_layer(fc_common, 1, activation_fn=None,name1="LASTFC")[:,0] #8 1
+        self.initial_state = None
+
+        # Take an action in the action distribution (remember we are in a situation
+        # of stochastic policy so we don't always take the action with the highest probability
+        # for instance if we have 2 actions 0.7 and 0.3 we have 30% chance to take the second)
+
+
+
+        # Function use to take a step returns action to take and V(s)
+        def step(state_in, *_args, **_kwargs):
+            sl,action, value= sess.run([softmax_layer,a0, vf], {inputs_: state_in})
+            print(sl)
+
+
+            # max=-1
+            # maxa=0
+            # for i in range(0,7):
+            #     if max<pi[0][i]:
+            #         max=pi[0][i]
+            #         maxa=i
+            # print("NOOP: ",pi[0][1])
+            # print("RIGHT: ",pi[0][1])
+            # print("RIGHTJUMP: ",pi[0][2])
+            # print("RIGHTFAST: ",pi[0][3])
+            # print("RIGHTJUMPFAST: ",pi[0][4])
+            # print("JUMP:",pi[0][5])
+            # print("LEFT: ",pi[0][6])
+
+
+
+
+
+
+            return action, value
+
+        # Function that calculates only the V(s)
+        def value(state_in, *_args, **_kwargs):
+            return sess.run(vf, {inputs_: state_in})
+
+        # Function that output only the action to take
+        def select_action(state_in, *_args, **_kwargs):
+
+            return sess.run(a0, {inputs_: state_in})
+
+        self.inputs_ = inputs_
+        self.vf = vf
+        self.step = step
+        self.value = value
+        self.select_action = select_action
