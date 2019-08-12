a4de81212f35804be86272450cd1e1aa4900fb73
Kimiya Saadat <kimiya.saadat97@gmail.com>
2019-08-12 18:58:27 +0430

    small changes to get back to normal state of no additional actions

--- model.py
+++ model.py
@@ -233,12 +233,7 @@ class Runner(AbstractEnvRunner):
             # Given observations, take action and value (V(s))
             # We already have self.obs because AbstractEnvRunner run self.obs[:] = env.reset()
             actions, values = self.model.step(self.obs, epsilon)
-            random=np.random.random_sample()
-
-            if(random<epsilon):
-
-                random_index=np.random.randint(7, size=12)
-                actions=random_index
+            
             if flag.DEBUG:
                 step_num_to_name(actions)
             # print(tf.is_numeric_tensor(actions))
@@ -339,7 +334,7 @@ def learn(policy,
     if flag.ON_DESKTOP:
         nminibatches = 1 #8
     else:
-        nminibatches = 8
+        nminibatches = 1
 
     if flag.ON_DESKTOP:
         noptepochs = 1  # 8
