aae6949246491f3a8d4c035806c6f81858df865d
Justkim <kimiya.saadat97@gmail.com>
2019-08-28 01:19:03 +0430

    reduced workers to one

--- agent.py
+++ agent.py
@@ -35,7 +35,8 @@ def main():
 
     else:
 
-        made_env=SubprocVecEnv([env.make_train_0, env.make_train_0, env.make_train_0, env.make_train_0, env.make_train_0, env.make_train_0, env.make_train_0, env.make_train_0, env.make_train_0, env.make_train_0, env.make_train_0, env.make_train_0,env.make_train_0,env.make_train_0,env.make_train_0,env.make_train_0])
+ #       made_env=SubprocVecEnv([env.make_train_0, env.make_train_0, env.make_train_0, env.make_train_0, env.make_train_0, env.make_train_0, #env.make_train_0, env.make_train_0, env.make_train_0, env.make_train_0, env.make_train_0, #env.make_train_0,env.make_train_0,env.make_train_0,env.make_train_0,env.make_train_0])
+        made_env = SubprocVecEnv([env.make_train_0])
         nsteps=128
 
 
